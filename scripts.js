a242f519257047ba4da0ac7c0349a4c4bcf17436
Gabriel Henrique <smartsbiel@gmail.com>
2023-02-13 12:44:02 -0300

    fisrt commit

---
+++ scripts.js
@@ -0,0 +1,6 @@
+window.sr = ScrollReveal({ reset:true});
+ScrollReveal({ distance: '10px' });
+sr.reveal('.infos', {duration : 3000, origin: 'right'})
+sr.reveal('.infos-2', {duration : 3000, origin: 'left'})
+sr.reveal('.cards', {duration : 3000,  origin: 'right'})
+sr.reveal('.footer', {duration : 3000, origin: 'left'})
